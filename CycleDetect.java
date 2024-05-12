f168cfa2593302126dc304f9497a421180b342dd
Alokmish11 <140834850+Alokmish11@users.noreply.github.com>
2024-05-12 16:45:57 +0530

    Add files via upload

---
+++ CycleDetect.java
@@ -0,0 +1,36 @@
+public class CycleDetect {
+    public static class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+public static boolean isCycle(){
+    Node slow=head;
+    Node fast=head;
+    while(fast!=null && fast.next!=null){
+    
+        slow =slow.next; //// +1
+        fast=fast.next.next;  // +2
+       if(slow==fast){
+        return true;
+       }
+    }
+   return false;
+}
+public static void main(String[] args){ 
+    head=new Node(1);
+    head.next=new Node(2);
+    head.next.next=new Node(3);
+    head.next.next.next=head;
+    System.out.println(isCycle());
+
+
+}
+}
+
